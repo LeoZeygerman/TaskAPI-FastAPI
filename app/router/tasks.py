9cafd09639481e5d59964ca886cfedf0d844aae5
BBYBOI <zeygermanleo@gmail.com>
2026-09-01 22:00:01 +0500

    get one book

--- app/router/tasks.py
+++ app/router/tasks.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from sqlalchemy import select
 from app.database import SessionDep
 from app.models import TaskOrm
-from app.schemas import CreateTask
+from app.schemas import CreateTask, ResponseTask
 
 router = APIRouter(prefix='/tasks', tags=['Задачи'])
 
@@ -18,3 +19,12 @@ async def add_task(session: SessionDep, task: CreateTask):
     await session.commit()
 
     return {'msg': f'Задача {task.task_title} добавлена!'}
+
+@router.get('/get_task/{task_id}', summary= 'Получить одну задачу', response_model=ResponseTask)
+async def get_one_task(session: SessionDep, task_id: int):
+    query = select(TaskOrm).where(task_id = TaskOrm.id)
+    result = await session.execute(query)
+    book = result.scalar_one_or_none()
+    if book is None:
+        raise HTTPException(status_code=404, detail='Книга не найдена')
+    return book
